d0ca581aa719ed5d342c775c59cc3a531f1cba11
Marcus Völker <marcus.voelker@rwth-aachen.de>
2020-12-03 10:41:18 +0100

    Day 3

---
+++ Day3.hs
@@ -0,0 +1,18 @@
+module Day3 where
+
+loadData :: IO [[Bool]]
+loadData = do
+    dat <- readFile "Day3.dat"
+    return $ map (cycle . map (=='#')) $ lines dat
+
+slopeCheck :: Int -> Int -> [[Bool]] -> Int
+slopeCheck x y = length . filter (\(i,r) -> r!!(div i y*x) && mod i y == 0) . zip [0..]
+
+part1 :: IO ()
+part1 = loadData >>= (print . slopeCheck 3 1)
+
+part2 :: IO ()
+part2 = do
+    dat <- loadData
+    let res = product$ map (\(x,y) -> slopeCheck x y dat) [(1,1),(3,1),(5,1),(7,1),(1,2)]
+    print res
