88d9f97c43d855f3fe6211cb365e1096371dca1f
Marcus Völker <voelker@embedded.rwth-aachen.de>
2020-12-14 14:09:08 +0100

    Day14 done

--- Day14.hs
+++ Day14.hs
@@ -5,6 +5,7 @@ module Day14 where
 import Control.Lens
 import Data.Bits
 import qualified Data.Map as M
+import qualified Data.Set as S
 
 data Instr = Mask (Integer, Integer) | Mem Integer Integer deriving (Show)
 
@@ -21,13 +22,20 @@ runInstr1 (Mem a i) s = s & memory %~ M.insert a (masked (s ^. mask) i)
 
 runInstr2 :: Instr -> State -> State
 runInstr2 (Mask i) s = s & mask .~ i
-runInstr2 (Mem a i) s = s & memory %~ (\m -> foldr (`M.insert` i) m (instantiate (s ^. mask)))
+runInstr2 (Mem a i) s = s & memory %~ (\m -> foldr (`M.insert` i) m (instantiate a (s ^. mask)))
 
-instantiate :: (Integer, Integer) -> [Integer]
-instantiate (z, o) =
-  let count = abs (popCount (complement z .|. o))
-      mask = complement z .|. o
-   in take (2 ^ count) $ map (.&. complement mask) $ iterate (\x -> (x + 1) .|. mask) mask
+uniquePrefix :: (Eq a, Ord a) => [a] -> [a]
+uniquePrefix = uniquePrefix' S.empty
+  where
+    uniquePrefix' s (x : xs)
+      | x `elem` s = []
+      | otherwise = x : uniquePrefix' (S.insert x s) xs
+
+instantiate :: Integer -> (Integer, Integer) -> [Integer]
+instantiate a (z, o) =
+  let mask = complement z .|. o
+      ws = uniquePrefix $ map (.&. complement mask) $ iterate (\x -> (x + 1) .|. mask) mask
+   in map (\w -> (mask .&. a) .|. o .|. w) ws
 
 repX :: Char -> String -> String
 repX _ [] = []
