220f0c9aa9b4ef20ccfcdfbe4af8586894889c44
Marcus Völker <voelker@embedded.rwth-aachen.de>
2020-12-14 06:53:47 +0100

    Day14 p1

---
+++ Day14.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Day14 where
+
+import Control.Lens
+import Data.Bits
+import qualified Data.Map as M
+
+data Instr = Mask (Integer, Integer) | Mem Int Integer deriving (Show)
+
+data State = State {_mask :: (Integer, Integer), _memory :: M.Map Int Integer} deriving (Show)
+
+makeLenses ''State
+
+masked :: (Integer, Integer) -> Integer -> Integer
+masked (z, o) i = (i .&. z) .|. o
+
+runInstr1 :: Instr -> State -> State
+runInstr1 (Mask i) s = s & mask .~ i
+runInstr1 (Mem a i) s = s & memory %~ M.insert a (masked (s ^. mask) i)
+
+runInstr2 :: Instr -> State -> State
+runInstr2 (Mask i) s = s & mask .~ i
+runInstr2 (Mem a i) s = s & memory %~ (\m -> foldr (`M.insert` i) m (instantiate (s ^. mask)))
+
+instantiate :: (Integer, Integer) -> [Int]
+instantiate (z, o) = undefined
+
+repX :: Char -> String -> String
+repX _ [] = []
+repX c ('X' : xs) = c : repX c xs
+repX c (x : xs) = x : repX c xs
+
+readBin :: String -> Integer
+readBin = readBin' 0
+  where
+    readBin' r [] = r
+    readBin' r ('0' : xs) = readBin' (2 * r) xs
+    readBin' r ('1' : xs) = readBin' (2 * r + 1) xs
+
+parseInstr :: String -> Instr
+parseInstr s
+  | take 4 s == "mask" =
+    let s' = drop 7 s
+     in Mask (readBin (repX '1' s'), readBin (repX '0' s'))
+  | otherwise =
+    let s' = drop 4 s
+        (adr, r) = head (reads s')
+        r' = drop 4 r
+     in Mem adr (read r')
+
+loadData :: IO [Instr]
+loadData = do
+  dat <- readFile "Day14.dat"
+  return $ map parseInstr $ lines dat
+
+part1 :: IO ()
+part1 = do
+  dat <- loadData
+  print $ M.foldr (+) 0 $ _memory $ foldl (flip runInstr1) (State (0, 0) M.empty) dat
+
+part2 :: IO ()
+part2 = do
+  dat <- loadData
+  print $ M.foldr (+) 0 $ _memory $ foldl (flip runInstr2) (State (0, 0) M.empty) dat
+
+test = "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X\nmem[8] = 11\nmem[7] = 101\nmem[8] = 0"
